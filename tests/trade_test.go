1d31e0aaa8bf998771e710e88498b71a23edb978
sleep <sleep@kaituocn.com>
2025-06-22 23:03:37 +0800

    modify ext

--- tests/trade_test.go
+++ tests/trade_test.go
@@ -1,20 +1,21 @@
-package trade
+package tests
 
 import (
 	"testing"
 
 	"github.com/huifurepo/bspay-go-sdk/ut/tool"
+	"github.com/sleep-go/huifu-pay/api/trade"
 	"github.com/sleep-go/huifu-pay/common"
 )
 
-var tr *Trade
+var tr *trade.Trade
 
 func init() {
 	huifuPay := common.NewHuifuPay(true, "../../config/config.json")
-	tr = NewTrade(huifuPay)
+	tr = trade.NewTrade(huifuPay)
 }
 func TestJSpay(t *testing.T) {
-	jspay, raw, err := tr.V3TradePaymentJspay(V3TradePaymentJspayRequest{
+	jspay, raw, err := tr.V3TradePaymentJspay(trade.V3TradePaymentJspayRequest{
 		ReqDate:   tool.GetCurrentDate(),
 		ReqSeqId:  tool.GetReqSeqId(),
 		HuifuId:   tr.HuifuPay.BsPay.Msc.SysId,
@@ -22,7 +23,7 @@ func TestJSpay(t *testing.T) {
 		GoodsDesc: "GoodsDesc",
 		TradeType: common.TradeType_A_NATIVE,
 		TransAmt:  "0.01",
-		ExtendInfos: V3TradePaymentJspayExtendInfos{
+		ExtendInfos: trade.V3TradePaymentJspayExtendInfos{
 			NotifyUrl: "",
 		},
 	})
