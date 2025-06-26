10216fba1f475b7aedd4c0389127f6d4b138f535
sleep <sleep@kaituocn.com>
2025-06-26 14:05:09 +0800

    AcctSplitBunch 结构调整

--- api/trade/payment_jspay.go
+++ api/trade/payment_jspay.go
@@ -230,7 +230,7 @@ type V3TradePaymentJspayExtendInfos struct {
 	DcData                DcData                `json:"dc_data"` //数字人民币参数集合
 	DelayAcctFlag         string                `json:"delay_acct_flag"`
 	FeeFlag               string                `json:"fee_flag"`
-	AcctSplitBunch        AcctSplitBunch        `json:"acct_split_bunch,omitempty"`
+	AcctSplitBunch        AcctSplitBunch        `json:"acct_split_bunch"`
 	TermDivCouponType     string                `json:"term_div_coupon_type"` //传入分账遇到优惠的处理规则	Integer	1	N	1: 按比例分，2: 按分账明细顺序保障，3: 只给交易商户（默认)；示例值：1
 	CombinedpayData       []CombinedpayData     `json:"combinedpay_data"`
 	LimitPayType          string                `json:"limit_pay_type"` //禁用信用卡标记	String	128	N	本次交易禁止使用的支付方式，默认不禁用；取值参见说明；示例值：NO_CREDIT
