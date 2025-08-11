f8f0764cb7cf2d88f2f318b1e562d01f44f8bcb8
sleep <sleep@kaituocn.com>
2025-08-11 14:24:50 +0800

    V2UserBasicdataIndv 用户入驻 字段调整

--- api/user/basicdata_indv.go
+++ api/user/basicdata_indv.go
@@ -44,22 +44,23 @@ type V2UserBasicdataIndvRequest struct {
 	CertNationality  string          `json:"cert_nationality"`   //个人国籍	String	50	C	个人证件类型为外国人居留证时，必填，参见《国籍编码》示例值：CHN
 	MobileNo         string          `json:"mobile_no"`          //手机号	String	11	Y	示例值：13917354627
 	Address          string          `json:"address"`            //地址	String	256	C	开通中信E管家必填
-	ExtendInfos      struct {
-		CertEndDate string //个人证件有效期截止日期	String	8	N	日期格式：yyyyMMdd; 示例值：20330909 ；长期有效时可不填，非长期有效必填
-		Email       string //电子邮箱	String	64	N	示例值：carl.chen@huifu.com
-		LoginName   string //管理员账号	String	32	N	示例值：Lg2022022201394910571
-		SmsSendFlag string //是否发送短信标识	String	1	N	Y:发送短信通知，N：不发送短信通知。默认不发送短信通知。示例值：Y
-		ExpandId    string //拓展方字段	String	18	N	如果该商户是第三方展业的可以填写拓展方的huifu_id;示例值：6666000123123123
-		FileList    []struct {
-			FileId   string //文件jfileID	String	128	Y	图片上传接口生成的fileId；示例值：57cc7f00-600a-33ab-b614-6221bbf2e529
-			FileName string //文件名称	String	128	N	示例值：test42001.jpg
-			FileType string //文件类型	String	8	Y	参见文件类型；示例值：F85
-		} //文件列表
-		Mcc        string //	所属行业	String	7	N	参考汇付MCC编码 ；示例值：5311； 当用户业务入驻修改，电子回单配置开关为开通时，需填写
-		ProvId     string //省	String	6	N	参考地区编码；示例值：310000 ；如修改省市区要级联修改； 当用户业务入驻修改，电子回单配置开关为开通时，需填写
-		AreaId     string //市	String	6	N	参考地区编码；示例值：310000 ；如修改省市区要级联修改； 当用户业务入驻修改，电子回单配置开关为开通时，需填写
-		DistrictId string //区	String	6	N	参考地区编码；示例值：310101 ；如修改省市区要级联修改； 当用户业务入驻修改，电子回单配置开关为开通时，需填写
-	}
+	ExtendInfos      V2UserBasicdataIndvExtendInfos
+}
+type V2UserBasicdataIndvExtendInfos struct {
+	CertEndDate string `json:"cert_end_date"` //个人证件有效期截止日期	String	8	N	日期格式：yyyyMMdd; 示例值：20330909 ；长期有效时可不填，非长期有效必填
+	Email       string `json:"email"`         //电子邮箱	String	64	N	示例值：carl.chen@huifu.com
+	LoginName   string `json:"login_name"`    //管理员账号	String	32	N	示例值：Lg2022022201394910571
+	SmsSendFlag string `json:"sms_send_flag"` //是否发送短信标识	String	1	N	Y:发送短信通知，N：不发送短信通知。默认不发送短信通知。示例值：Y
+	ExpandId    string `json:"expand_id"`     //拓展方字段	String	18	N	如果该商户是第三方展业的可以填写拓展方的huifu_id;示例值：6666000123123123
+	FileList    []struct {
+		FileId   string `json:"file_id"`   //文件jfileID	String	128	Y	图片上传接口生成的fileId；示例值：57cc7f00-600a-33ab-b614-6221bbf2e529
+		FileName string `json:"file_name"` //文件名称	String	128	N	示例值：test42001.jpg
+		FileType string `json:"file_type"` //文件类型	String	8	Y	参见文件类型；示例值：F85
+	} `json:"file_list"` //文件列表
+	Mcc        string `json:"mcc"`         //	所属行业	String	7	N	参考汇付MCC编码 ；示例值：5311； 当用户业务入驻修改，电子回单配置开关为开通时，需填写
+	ProvId     string `json:"prov_id"`     //省	String	6	N	参考地区编码；示例值：310000 ；如修改省市区要级联修改； 当用户业务入驻修改，电子回单配置开关为开通时，需填写
+	AreaId     string `json:"area_id"`     //市	String	6	N	参考地区编码；示例值：310000 ；如修改省市区要级联修改； 当用户业务入驻修改，电子回单配置开关为开通时，需填写
+	DistrictId string `json:"district_id"` //区	String	6	N	参考地区编码；示例值：310101 ；如修改省市区要级联修改； 当用户业务入驻修改，电子回单配置开关为开通时，需填写
 }
 type V2UserBasicdataIndvResponse struct {
 	Data struct {
--- tests/user_test.go
+++ tests/user_test.go
@@ -32,31 +32,16 @@ func TestV2UserBasicdataIndv(t *testing.T) {
 		CertBeginDate:    "",
 		MobileNo:         "",
 		Address:          "",
-		ExtendInfos: struct {
-			CertEndDate string //个人证件有效期截止日期	String	8	N	日期格式：yyyyMMdd; 示例值：20330909 ；长期有效时可不填，非长期有效必填
-			Email       string //电子邮箱	String	64	N	示例值：carl.chen@huifu.com
-			LoginName   string //管理员账号	String	32	N	示例值：Lg2022022201394910571
-			SmsSendFlag string //是否发送短信标识	String	1	N	Y:发送短信通知，N：不发送短信通知。默认不发送短信通知。示例值：Y
-			ExpandId    string //拓展方字段	String	18	N	如果该商户是第三方展业的可以填写拓展方的huifu_id;示例值：6666000123123123
-			FileList    []struct {
-				FileId   string //文件jfileID	String	128	Y	图片上传接口生成的fileId；示例值：57cc7f00-600a-33ab-b614-6221bbf2e529
-				FileName string //文件名称	String	128	N	示例值：test42001.jpg
-				FileType string //文件类型	String	8	Y	参见文件类型；示例值：F85
-			} //文件列表
-			Mcc        string //	所属行业	String	7	N	参考汇付MCC编码 ；示例值：5311； 当用户业务入驻修改，电子回单配置开关为开通时，需填写
-			ProvId     string //省	String	6	N	参考地区编码；示例值：310000 ；如修改省市区要级联修改； 当用户业务入驻修改，电子回单配置开关为开通时，需填写
-			AreaId     string //市	String	6	N	参考地区编码；示例值：310000 ；如修改省市区要级联修改； 当用户业务入驻修改，电子回单配置开关为开通时，需填写
-			DistrictId string //区	String	6	N	参考地区编码；示例值：310101 ；如修改省市区要级联修改； 当用户业务入驻修改，电子回单配置开关为开通时，需填写
-		}{
+		ExtendInfos: user.V2UserBasicdataIndvExtendInfos{
 			CertEndDate: "",
 			Email:       "",
 			LoginName:   "",
 			SmsSendFlag: "",
 			ExpandId:    "",
 			FileList: []struct {
-				FileId   string
-				FileName string
-				FileType string
+				FileId   string `json:"file_id"`
+				FileName string `json:"file_name"`
+				FileType string `json:"file_type"`
 			}{
 				{
 					FileId:   "123",
